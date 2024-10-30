7bc52d9451dd49952c892f7a7593fe2eb30f12ce
DavidPatino8 <744012@student.dusd.net>
2024-10-30 15:23:58 -0700

    World Collisions
    Added a rigid body to bread and Marshmallow. Need to edit the collision box of Marshmallow.

--- 2DBeginnerAdventureP8Morales/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Morales/Assets/Scripts/PlayerController.cs
@@ -5,28 +5,31 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.01f * horizontal;
-        position.y = position.y + 0.01f * vertical;
-
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    
     }
-     
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 0.4f * horizontal;
+        position.y = position.y + 0.4f * vertical;
 
-    
+        rigidbody2d.MovePosition(position);
+    }
 
-    
 
 }
